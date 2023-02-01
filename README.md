76aec2c7add564d00ab1ef7f873ef9b1698d5305
gamecss <52902973+gamecss@users.noreply.github.com>
2023-02-01 13:57:12 +0800

    Initial

---
+++ README.md
@@ -0,0 +1,2 @@
+# raws
+some raw file
---
+++ j6env.sh
@@ -0,0 +1,4 @@
+export JAVA_HOME=/usr/local/jdk1.6.0_45
+export JRE_HOME=${JAVA_HOME}/jre
+export CLASSPATH=.:${JAVA_HOME}/lib:${JRE_HOME}/lib
+export PATH=${JAVA_HOME}/bin:$PATH
